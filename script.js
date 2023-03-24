6f0720028fb8905f67ca95a18c98174a1ed6f96d
Maksym Titenko <Titenko.m@gmail.com>
2023-03-24 15:47:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,22 @@ fetch(apiUrl)
       const a = document.createElement("a");
       a.href = fileUrl;
       a.innerText = fileName;
+      // добавляем обработчик событий для загрузки файла
+      a.addEventListener("click", e => {
+        e.preventDefault(); // отменяем стандартное поведение ссылки
+        fetch(fileUrl)
+          .then(response => response.blob())
+          .then(blob => {
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = fileName;
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+          })
+          .catch(error => console.error(error));
+      });
       li.appendChild(a);
       fileList.appendChild(li);
     }
