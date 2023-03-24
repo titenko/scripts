e92a8d749d8ce54ffb319e2652c95c2d4f0cff46
Maksym Titenko <Titenko.m@gmail.com>
2023-03-24 16:19:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
 const apiUrl = "https://api.github.com/repos/titenko/scripts/contents";
 
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    // обработка JSON-ответа API GitHub
-    const fileList = document.getElementById("fileList"); // получаем элемент для вывода списка файлов
-    for (const item of data) {
-      const fileName = item.name;
-      const fileUrl = item.download_url;
-      const fileType = item.type;
-      // создаем новый элемент списка файлов на странице
-      const li = document.createElement("li");
-      const a = document.createElement("a");
-      a.href = fileUrl;
-      a.innerText = fileName;
-      // добавляем класс для папок и файлов
-      if (fileType === "dir") {
-        li.classList.add("folder");
-      } else {
-        li.classList.add("file");
-      }
-      // добавляем обработчик событий для загрузки файла
-      a.addEventListener("click", e => {
-        e.preventDefault(); // отменяем стандартное поведение ссылки
-        fetch(fileUrl)
-          .then(response => response.blob())
-          .then(blob => {
-            const url = URL.createObjectURL(blob);
-            const a = document.createElement("a");
-            a.href = url;
-            a.download = fileName;
-            document.body.appendChild(a);
-            a.click();
-            a.remove();
-          })
-          .catch(error => console.error(error));
-      });
-      li.appendChild(a);
-      fileList.appendChild(li);
+const createFileElement = ({name, download_url}) => {
+  const a = document.createElement("a");
+  a.href = download_url;
+  a.innerText = name;
+  return a;
+};
+
+const createFolderElement = (name, childrenElements) => {
+  const ul = document.createElement("ul");
+  ul.classList.add("folder");
+  const li = document.createElement("li");
+  li.innerText = name;
+  li.appendChild(ul);
+  for (const child of childrenElements) {
+    const childLi = document.createElement("li");
+    childLi.appendChild(child);
+    ul.appendChild(childLi);
+  }
+  return li;
+};
+
+const getFilesAndFolders = async (path) => {
+  const response = await fetch(`${apiUrl}/${path}`);
+  const data = await response.json();
+  const files = [];
+  const folders = [];
+  for (const item of data) {
+    const name = item.name;
+    const download_url = item.download_url;
+    if (item.type === "file") {
+      files.push(createFileElement({name, download_url}));
+    } else if (item.type === "dir") {
+      const children = await getFilesAndFolders(`${path}/${name}`);
+      folders.push(createFolderElement(name, children));
     }
-  })
-  .catch(error => console.error(error));
+  }
+  return [...folders, ...files];
+};
+
+const main = async () => {
+  const fileList = document.getElementById("fileList");
+  const elements = await getFilesAndFolders("");
+  for (const element of elements) {
+    fileList.appendChild(element);
+  }
+};
+
+main().catch(error => console.error(error));
